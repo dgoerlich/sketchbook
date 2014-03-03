80d69b4eef161cba5ef3cc1f72b75cadc7761f06
Dave Goerlich <dgoerlich@designinginteractive.com>
2014-03-03 12:29:43 -0500

    Based on some example code, cross fade now works with good math

--- fade_color_mix/fade_color_mix.ino
+++ fade_color_mix/fade_color_mix.ino
@@ -1,76 +1,110 @@
-const int redLedPin = 9;
-const int greenLedPin = 10;
-const int blueLedPin = 11;
+const int redPin = 9;
+const int greenPin = 10;
+const int bluePin = 11;
 
-const int fadeSeconds = 3;
-const int fadeSteps = 30;
+const int fadeStepInterval = 2;
+
+int red = 0;
+int green = 0;
+int blue = 0;
+
+int prevRed = red;
+int prevGreen = green;
+int prevBlue = blue;
 
 void setup()
 {
   // initialize the serial communication:
   Serial.begin(9600);
-  pinMode(redLedPin, OUTPUT);
-  pinMode(greenLedPin, OUTPUT);
-  pinMode(blueLedPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
 }
 
 void loop() {
   char buffer[3];
-
-  static int red = 0;
-  static int green = 0;
-  static int blue = 0;
-
-  int redDelta;
-  int greenDelta;
-  int blueDelta;
+  byte color[3];
   
-  float redIncrement;
-  float greenIncrement;
-  float blueIncrement;
-  
-  float redPartial;
-  float greenPartial;
-  float bluePartial;
-
   // check if data has been sent from the computer:
   if (Serial.available()) {
      Serial.readBytes(buffer, 3);
+  
+     color[0] = byte(buffer[0]);
+     color[1] = byte(buffer[1]);
+     color[2] = byte(buffer[2]);
      
-     //Serial.print(red);
-     //Serial.print(green);
-     //Serial.println(blue);
-     
-     redDelta = buffer[0] - red;
-     greenDelta = buffer[1] - green;
-     blueDelta = buffer[2] - blue;
-     
-     redIncrement = float(redDelta) / fadeSteps;
-     greenIncrement = float(greenDelta) / fadeSteps;
-     blueIncrement = float(blueDelta) / fadeSteps;
-     
-     redPartial = red;
-     greenPartial = green;
-     bluePartial = blue;
-
-     for (int x = 0; x < fadeSteps; x++) {
-       redPartial += redIncrement;
-       greenPartial += greenIncrement;
-       bluePartial += blueIncrement;
-
-       analogWrite(redLedPin, redPartial);
-       analogWrite(greenLedPin, greenPartial);
-       analogWrite(blueLedPin, bluePartial);
-      
-       delay((fadeSeconds * 1000) / fadeSteps);
-     }    
-     
-     red = buffer[0];
-     green = buffer[1];
-     blue = buffer[2];
-
-     analogWrite(redLedPin, red);
-     analogWrite(greenLedPin, green);
-     analogWrite(blueLedPin, blue);
+     crossFade(color);
   }  
 }
+
+int calculateStep(int prevValue, int endValue) {
+  int step = endValue - prevValue; // What's the overall gap?
+  if (step) {                      // If its non-zero, 
+    step = 1020/step;              //   divide by 1020
+  } 
+  return step;
+}
+
+int calculateVal(int step, int val, int i) {
+
+  if ((step) && i % step == 0) { // If step is non-zero and its time to change a value,
+    if (step > 0) {              //   increment the value if step is positive...
+      val += 1;           
+    } 
+    else if (step < 0) {         //   ...or decrement it if step is negative
+      val -= 1;
+    } 
+  }
+  // Defensive driving: make sure val stays in the range 0-255
+  if (val > 255) {
+    val = 255;
+  } 
+  else if (val < 0) {
+    val = 0;
+  }
+  return val;
+}
+
+void crossFade(byte color[3]) {
+  int stepR = calculateStep(prevRed, color[0]);
+  int stepG = calculateStep(prevGreen, color[1]); 
+  int stepB = calculateStep(prevBlue, color[2]);
+
+  Serial.print("Target: ");
+  Serial.print(int(color[0]));
+  Serial.print(" / ");
+  Serial.print(int(color[1]));
+  Serial.print(" / ");  
+  Serial.println(int(color[2]));
+
+  for (int i = 0; i <= 1020; i++) {
+    red = calculateVal(stepR, red, i);
+    green = calculateVal(stepG, green, i);
+    blue = calculateVal(stepB, blue, i);
+
+    analogWrite(redPin, red);   // Write current values to LED pins
+    analogWrite(greenPin, green);      
+    analogWrite(bluePin, blue); 
+
+    if (i == 0 or i % 60 == 0) { // beginning, and every loopCount times
+        Serial.print("Loop/RGB: #");
+        Serial.print(i);
+        Serial.print(" | ");
+        Serial.print(red);
+        Serial.print(" / ");
+        Serial.print(green);
+        Serial.print(" / ");  
+        Serial.println(blue); 
+      } 
+
+    delay(fadeStepInterval); // Pause for 'wait' milliseconds before resuming the loop
+  }
+  
+  // Update current values for next loop
+  prevRed = red; 
+  prevGreen = green; 
+  prevBlue = blue;
+  
+  Serial.println("DONE");
+}
+
